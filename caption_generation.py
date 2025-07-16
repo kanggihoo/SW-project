11cb3d3c2891c331b48068e5c83c2dadea5e5ebc
kkh <11kkh19999.gmail.com>
2025-07-16 15:43:51 +0900

    caption_generation.py pipe 수정

--- caption_generation.py
+++ caption_generation.py
@@ -4,13 +4,25 @@ from aws.aws_manager import AWSManager
 from processing.image_processor import download_images_sync , parsing_data_for_llm 
 from caption.models.product import ImageManager, ProductManager, Base64DataForLLM
 from caption.fashion_caption_generator import FashionCaptionGenerator
+
+from db.repository.fashion import FashionRepository
+from db.config.database import DatabaseManager
 import json 
+from db.config import Config
 from utils import setup_logger
 
-logger = setup_logger()
-logger.info("test")
+logger = logging.getLogger(__name__)
 if __name__ == "__main__":
+    db_config = Config()
     aws_manager = AWSManager()
+
+    mongodb_client = DatabaseManager(connection_string=db_config["MONGODB_LOCAL_CONNECTION_STRING"] , database_name=db_config["MONGODB_LOCAL_DATABASE_NAME"] , collection_name=db_config["MONGODB_LOCAL_COLLECTION_NAME"])
+    mongodb_atlas_client = DatabaseManager(connection_string=db_config["MONGODB_ATLAS_CONNECTION_STRING"] , database_name=db_config["MONGODB_ATLAS_DATABASE_NAME"] , collection_name=db_config["MONGODB_ATLAS_COLLECTION_NAME"])
+    collection_local = mongodb_client.get_collection()
+    collection_atlas = mongodb_atlas_client.get_collection()
+    fashion_repository = FashionRepository(collection=collection_local)
+
+
     fashion_caption_generator = FashionCaptionGenerator()
     pagenator = aws_manager.dynamodb_manager.get_product_pagenator(sub_category=1005 , condition={"curation_status":"COMPLETED"})
     count = 0
@@ -19,7 +31,7 @@ if __name__ == "__main__":
         logger.info(f"현재 총 제품 수 : {page.get('Count')}")
         count += page.get('Count')
         if items:
-            # 여기 for문은 좀더 효율적으로 돌릴 수 있을 거 같은데 
+            #TODO 여기 for문은 좀더 효율적으로 돌릴 수 있을 거 같은데 
             for item in items:
                 logger.info(f"main_category : {item.get('main_category')} , sub_category : {item.get('sub_category')} , 제품 id : {item.get('product_id')} , 제품 이름 : {item.get('product_name')}")
                 main_category = item.get('main_category')
@@ -36,13 +48,19 @@ if __name__ == "__main__":
                 color_images = result.get("color_images").model_dump()
                 text_images = result.get("text_images").model_dump_json()
 
-                # dynamodb 반영 
+                # dynamodb 반영 (caption PENDING => COMPLETED)
                 aws_manager.dynamodb_manager.update_caption_result(sub_category, product_id, "COMPLETED")
         
-                # csv 파일에서 product_id로 부터 추가 정보 가져오기 (json으로 저장할 이유가 없네 , csv 파일로 빠르게 제품 id를 인덱스로 해서 빠르게 찾을 수 있게 해야지)
+                #mongodb 로 부터 데이터 가져오기 _id로 접근 
+                doc = fashion_repository.find_by_id(product_id)
 
-                # 최종 데이터 분리 (denormalize 및 대표 이미지 색상 필드 추가 및 product_id + 색상명으로 유니크 키 생성)
-
-                # 최종 데이터 저장 mongodb 저장 => 이후에 벡터 필드에 대한 임베딩 벡터 추가 
+                #TODO : mongoatlas에 저장할 필드만 가져오고 ,  # 최종 데이터 분리 (denormalize 및 대표 이미지 색상 필드 추가 및 product_id + 색상명으로 유니크 키 생성)
+            
+                # 최종 데이터 저장 mongodb atlas 저장 => 이후에 벡터 필드에 대한 임베딩 벡터 추가 
+                '''
+                - product_id , 
+                - 
+                
+                '''
 
                 
