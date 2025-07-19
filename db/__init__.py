2fb86db36fcf1d67f353114ff92396caae2c0fae
kkh <11kkh19999.gmail.com>
2025-07-19 14:26:37 +0900

    db module 구조 정리

--- db/__init__.py
+++ db/__init__.py
@@ -1,18 +1,16 @@
 from .config.config import Config
 from .repository.fashion import FashionRepository
 
-
+_config = Config()
 def create_fashion_repo_atlas():
-    config = Config()
-    return FashionRepository(connection_string=config["MONGODB_ATLAS_CONNECTION_STRING"],
-                            database_name=config["MONGODB_ATLAS_DATABASE_NAME"],
-                            collection_name=config["MONGODB_ATLAS_COLLECTION_NAME"])
+    return FashionRepository(connection_string=_config["MONGODB_ATLAS_CONNECTION_STRING"],
+                            database_name=_config["MONGODB_ATLAS_DATABASE_NAME"],
+                            collection_name=_config["MONGODB_ATLAS_COLLECTION_NAME"])
 
 def create_fashion_repo_local():
-    config = Config()
-    return FashionRepository(connection_string=config["MONGODB_LOCAL_CONNECTION_STRING"],
-                            database_name=config["MONGODB_LOCAL_DATABASE_NAME"],
-                            collection_name=config["MONGODB_LOCAL_COLLECTION_NAME"])
+    return FashionRepository(connection_string=_config["MONGODB_LOCAL_CONNECTION_STRING"],
+                            database_name=_config["MONGODB_LOCAL_DATABASE_NAME"],
+                            collection_name=_config["MONGODB_LOCAL_COLLECTION_NAME"])
 
 def create_fashion_repo(use_atlas: bool = False):
     if use_atlas:
