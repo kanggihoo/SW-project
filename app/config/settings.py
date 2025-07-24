c00f453f2bfd42c980a359ed0697b804f299fdaa
kkh <11kkh19999.gmail.com>
2025-07-24 16:08:35 +0900

    ec2 배포위한 yaml 파일 작성 및 환경변수 설정

--- app/config/settings.py
+++ app/config/settings.py
@@ -3,10 +3,12 @@ from functools import lru_cache
 from pydantic_settings import BaseSettings , SettingsConfigDict
 from pydantic import Field
 from typing import Annotated
-
+from db.config.config import Config as DBConfig
+from aws.config import Config as AWSConfig
+import os
 class Settings(BaseSettings):
     USE_ATLAS : Annotated[bool , Field(default=True)]
-    MONGODB_ATLAS_URI: Annotated[str , Field(... , json_schema_extra={"env": "MONGODB_ATLAS_URI"})]
+    MONGODB_ATLAS_URI: Annotated[str , Field(default=os.environ.get("MONGODB_ATLAS_URI") , json_schema_extra={"env": "MONGODB_ATLAS_URI"})]
     MONGODB_ATLAS_DATABASE: Annotated[str , Field(default="fashion_db")]
     MONGODB_ATLAS_COLLECTION: Annotated[str , Field(default="products")]
 
