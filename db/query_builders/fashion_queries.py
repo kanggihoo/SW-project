2e86fcc9811364258bb0bf3ec4741134d3405ff5
kkh <11kkh19999.gmail.com>
2025-07-24 13:55:43 +0900

    리팩토링 중 브랜치 이동전 commit

--- db/query_builders/fashion_queries.py
+++ db/query_builders/fashion_queries.py
@@ -1,78 +1,88 @@
-#TODO : 쿼리에 대한 결과 반환 메서드 추가 (find 매서드)
-#TODO : VectorSearch를 위한 메서드 추가 (sample_mflix.movies로 테스트 해보기) 벡터 서치를 위한 미리 생성된 
 import logging
 from typing import List, Dict , Callable
-logger = logging.getLogger(__name__)
+from db.config.config import Config
+
 
 '''
 원하는 데이터를 가져오기 위해서 mongodb 쿼리로 변환
-
 '''
 class FashionQueryBuilder:
     """MongoDB Atlas Search 를 사용한 쿼리/벡터 검색 파이프라인 생성
     Fashion 관련 쿼리/파이프라인 생성
     """
     def __init__(self):
-        pass
+        self.config = Config()
+        self.vector_search_config = self.config.get_vector_search_config()
+        
 
-    def caption_status_filter(self , caption_status: str) -> dict:
+    def caption_status_filter(self , caption_status: str="COMPLETED") -> dict:
         return {"caption_info.caption_status": caption_status}
     
-    # def get_active_users_pipeline(self) -> List[Dict]:
-    #     return [
-    #         {"$match": {"status": "active"}},
-    #         {"$sort": {"created_at": -1}},
-    #         {"$limit": 100}
-    #     ]
-    
-    # def get_user_stats_pipeline(self, user_id: str) -> List[Dict]:
-    #     return [
-    #         {"$match": {"_id": user_id}},
-    #         {"$lookup": {
-    #             "from": "orders",
-    #             "localField": "_id",
-    #             "foreignField": "user_id",
-    #             "as": "orders"
-    #         }},
-    #         {"$addFields": {
-    #             "order_count": {"$size": "$orders"},
-    #             "total_spent": {"$sum": "$orders.amount"}
-    #         }}
-    #     ]
-    def vector_search_pipeline(self, user_query:str, embedding_factory:Callable[[str], list[float]]):
+    #TODO : 벡터 검색 효율성 및 정확성을 위한 사전 필터링 인자 추가 
+    def vector_search_pipeline(self,
+                                user_query:str,
+                                embedding_factory:Callable[[str], list[float]],
+                                num_candidates:int=100,
+                                index_name:str = None,
+                                embedding_field_path:str = None,
+                                limit:int=10)->List[Dict]:
+        """
+        Vector Search 파이프라인 생성
+
+        Args:
+            user_query (str): 사용자 쿼리
+            embedding_factory (Callable[[str], list[float]]): 임베딩 팩토리
+            num_candidates (int, optional): 후보 개수. Defaults to 100.
+            index_name (str, optional): 인덱스 이름. Defaults to None.
+            embedding_field_path (str, optional): 임베딩 필드 경로. Defaults to None.
+            similarity (str, optional): 유사도 계산 방법. Defaults to None.
+            limit (int, optional): 결과 개수. Defaults to 10.
+
+        Returns:
+            List[Dict]: 검색 결과(유사도 점수 높은 순)
+        """
+        if not user_query and not isinstance(user_query, str):
+            raise ValueError("user_query 는 문자열이어야 합니다.")
+        
+        if not embedding_factory or not callable(embedding_factory):
+            raise ValueError("embedding_factory 는 함수여야 합니다.")
+        
+        # 기본 설정값 설정
+        index_name = index_name or self.vector_search_config.get("DEFAULT_VECTOR_INDEX")
+        embedding_field_path = embedding_field_path or self.vector_search_config.get("EMBEDDING_FIELD_PATH")
+        num_candidates = num_candidates or self.vector_search_config.get("DEFAULT_NUM_CANDIDATES")
+        limit = limit or self.vector_search_config.get("DEFAULT_LIMIT")
+
+
         embedding = embedding_factory(user_query)[0]
         pipeline = [
             {
                 "$vectorSearch": {
-                    "index": "tmp",
+                    "index": index_name,
                     "queryVector": embedding,
                     "exact": False,
-                    "path": "embedding.comprehensive_description.vector",
-                    "numCandidates": 100,
-                    "limit": 5,
-                }
-            },
-            {
-                "$project":{
-                    "product_id":1,
-                    "category_main":1,
-                    "category_sub":1,
-                    # "deep_caption":1,
-                    "representative_assets":1,
-                    # "caption_info":1,
-                    "score":{
-                        "$meta" : "vectorSearchScore"
-                    },   
+                    "path": embedding_field_path,
+                    "numCandidates": num_candidates,
+                    "limit": limit,
                 }
             }
-            # },
-            # {
-            #     "$replaceRoot": { "newRoot": { "$mergeObjects": ["$document", { "score": "$score" }] } }
-            #     # 'document' 필드를 루트로 승격하고, 'score' 필드를 병합
-            #     # 이렇게 하면 'score'가 최상위 필드로 포함된 원래 문서 형태가 됩니다.
-            # }
         ]
-        return pipeline 
-        # results = collection.aggregate(pipeline)
-        # for result in results:
-        #     logger.info(result)
+        # 기본 프로젝션값 설정 
+        project = {
+            "$project":self.vector_search_config.get("DEFAULT_PROJECT_FIELDS")
+        }
+        # 유사도 점수 추가 
+        project["$project"]["score"] = {
+            "$meta" : "vectorSearchScore"
+        }
+        pipeline.append(project)
+        return pipeline
+
+    def hybrid_search_pipeline(self,
+                               user_query:str,
+                               embedding_factory:Callable[[str], list[float]],
+                               num_candidates:int=100,
+                               index_name:str = None,
+                               embedding_field_path:str = None,
+                               limit:int=10)->List[Dict]:
+        ...
