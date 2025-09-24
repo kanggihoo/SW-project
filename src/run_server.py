a1111554d1e6798333e6c215d02f07ab5e532620
kkh <11kkh19999.gmail.com>
2025-09-25 03:54:52 +0900

    코드 리팩토링 및 postgre DB 연결

--- src/run_server.py
+++ src/run_server.py
@@ -4,4 +4,4 @@ from app.config.settings import settings
 
 if __name__ == "__main__":
     print(sys.path)
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload= not settings.is_dev() , log_level="debug")
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload= settings.is_dev() , log_level="debug")
